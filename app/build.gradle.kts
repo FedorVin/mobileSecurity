4321662ddfb031ef68804062c207abda767542ff
fedorvin <vino-f@pm.me>
2025-06-08 10:38:21 +0200

    empty app launches, most likely dependencies of the project added to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,4 +103,5 @@ dependencies {
     implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation ("org.json:json:20230227")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
+    implementation(libs.google.material)
 }
