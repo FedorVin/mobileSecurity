a5b3a752c80b42102941725e70ca02609bec410f
fedorvin <vino-f@pm.me>
2025-06-08 13:37:48 +0200

    changed more gradle stuff, currenrlty stuck on the same problem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     // Add kapt plugin for annotation processing
-//    id("kotlin-kapt")
+    // id("kotlin-kapt")
+    // id("com.google.devtools.ksp") version "1.9.20-1.0.14"
 
 }
 
@@ -52,6 +53,8 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     // Add Room annotation processor
     // kapt("androidx.room:room-compiler:2.6.1")
+    // ksp("androidx.room:room-compiler:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
     testImplementation("androidx.room:room-testing:2.6.1")
 
     // Core Android dependencies
